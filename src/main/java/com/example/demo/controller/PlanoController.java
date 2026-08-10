56ff510cfa7ecba9379787984b9daa8aab6d2055
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-08-09 21:06:40 -0300

    :construction:docs: Adicionando a entidade Plano a camada controller

---
+++ src/main/java/com/example/demo/controller/PlanoController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Plano;
+import com.example.demo.service.PlanoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planos")
+public class PlanoController {
+    private final PlanoService planoService;
+
+    public PlanoController(PlanoService planoService) {
+        this.planoService = planoService;
+    }
+
+    @PostMapping
+    public Plano cadastrarPlano(@RequestBody Plano plano){
+        return planoService.salvar(plano);
+    }
+
+    @GetMapping
+    public List<Plano> exibirPlanos(){
+        return planoService.buscarPlanos();
+    }
+}
