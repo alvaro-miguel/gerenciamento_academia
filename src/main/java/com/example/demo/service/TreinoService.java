0ed0a6eff36cc8a1836d377f2a4f7d2a7d2bfc8b
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-08-19 21:32:45 -0300

    :construction:docs: Implementando métodos específicos na entidade Treino

--- src/main/java/com/example/demo/service/TreinoService.java
+++ src/main/java/com/example/demo/service/TreinoService.java
@@ -1,17 +1,26 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Aluno;
+import com.example.demo.model.Professor;
 import com.example.demo.model.Treino;
+import com.example.demo.repository.AlunoRepository;
+import com.example.demo.repository.ProfessorRepository;
 import com.example.demo.repository.TreinoRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class TreinoService {
     private final TreinoRepository treinoRepository;
+    private final AlunoRepository alunoRepository;
+    private final ProfessorRepository professorRepository;
 
-    public TreinoService(TreinoRepository treinoRepository) {
+    public TreinoService(TreinoRepository treinoRepository, AlunoRepository alunoRepository, ProfessorRepository professorRepository) {
         this.treinoRepository = treinoRepository;
+        this.alunoRepository = alunoRepository;
+        this.professorRepository = professorRepository;
     }
 
     public Treino salvarTreino(Treino treino){
@@ -31,5 +40,25 @@ public class TreinoService {
         }
 
         treinoRepository.deleteById(id);
+    }
+
+    public void montarTreino(Treino treino, Long idAluno, Long idProfessor){
+        Aluno aluno = alunoRepository.findById(idAluno).orElseThrow(()
+                -> new IllegalArgumentException("Aluno inexistente"));
+
+        Professor professor = professorRepository.findById(idProfessor).orElseThrow(()
+        -> new IllegalArgumentException("Professor inexistente"));
+
+        treino.setAluno(aluno);
+        treino.setProfessor(professor);
+        treinoRepository.save(treino);
+    }
+
+    public void renovarVencimento(Long idTreino, LocalDate novaDataFim){
+        Treino treino = treinoRepository.findById(idTreino).orElseThrow(()
+                -> new IllegalArgumentException("Treino inexistente"));
+
+        treino.setDataFim(novaDataFim);
+        treinoRepository.save(treino);
     }
 }
